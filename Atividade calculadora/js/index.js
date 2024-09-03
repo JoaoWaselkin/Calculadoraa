fc42379e1c5bc103d65a35a452cb6fdda782bdb7
Waselkin <146507410+JoaoWaselkin@users.noreply.github.com>
2024-09-03 16:49:57 -0300

    Add files via upload

---
+++ Atividade calculadora/js/index.js
@@ -0,0 +1,35 @@
+
+function soma(a,b){
+    return a + b;
+}
+
+function subtracao(a,b){
+    return a - b;
+}
+
+function divisao(a,b){
+    if( b === 0){
+        return 'Nao é possivel fazer essa divisao'
+    }else
+        return a / b;
+}
+
+function multiplicacao(a,b){
+    return a * b;
+}
+
+function LimpaTela(){
+    let valor1 = ''
+    let valor2 = ''
+}
+
+console.log(divisao(10,5))
+
+
+
+/*function clearResult() {
+    document.getElementById('value1').value = '';
+    document.getElementById('value2').value = '';
+    document.getElementById('result').innerHTML = '0';
+}*/
+
